61d0b2c5419844f339348117768353be7edf2e90
Unnimaya T <Unnimaya.T@godigit.com>
2024-08-21 15:02:47 +0530

    2.setup- table

---
+++ EmployeeData/src/main/java/com/example/EmployeeData/model/Employee.java
@@ -0,0 +1,33 @@
+package com.example.EmployeeData.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "salary")
+    private double salary;
+}
